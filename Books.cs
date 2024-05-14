2fffd027af7a42f788434cfad3d83c40dd861579
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-05-14 23:53:30 +0500

    Create Book Class

---
+++ Books.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    internal class Books
+    {
+        public Books(string name)
+        {
+            this.name = name;
+        }
+
+        private string name;
+        private string author;
+        private string cost;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string Cost
+        {
+            get { return cost; }
+            set { cost = value; }
+        }
+
+    }
+}
